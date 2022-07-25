5568a9313aa1b80b53f984b1b2814f1b43f5a08e
Ivan Kazanovskiy <ivkazanovskiy@gmail.com>
2022-07-26 01:01:08 +0300

    fix: add simple server for deploy

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { config } from 'dotenv';
+import http from 'http';
 import { cron } from './cron';
 import { db } from './database/database';
 import { telegram } from './telegram';
@@ -9,3 +10,10 @@ db.initData().then(() => {
   telegram();
   //   cron();
 });
+
+http
+  .createServer((req, res) => {
+    res.write('Hello, world');
+    res.end();
+  })
+  .listen(process.env.PORT || 3000);
